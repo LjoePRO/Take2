2417c4a6043b83fd3ab8b48362b8789ef916e433
LjoePRO <ljprovencher@gmail.com>
2016-01-06 19:19:16 +1000

    Simple boolean expression

---
+++ src/Boolean.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Luke on 1/6/2016.
+ */
+public class Boolean {
+    public static void main(String[] args) {
+
+        System.out.println(false ||(8 < 9 || 7 == 7));
+
+    }
+}
